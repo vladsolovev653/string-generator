d508d621368b9f116d244ff7323e58227f6a6b22
Vlad Solovev <vladsolovev653@gmail.com>
2024-10-02 11:35:33 +0300

    add more validate

--- src/js/listeners.js
+++ src/js/listeners.js
@@ -31,29 +31,44 @@ strLength.addEventListener('input', (event) => {
   const value = Number(event.target.value);
 
   if (!value) {
+    // console.log(`${value} НЕ является числом`);
+    event.target.value = '';
     strLengthEmpty.style.display = 'inline';
     strLengthError.style.display = 'none';
     strLength.classList.remove('invalid');
     strLengthEmpty.textContent = `Значение от ${MIN_STR_LENGTH} до ${MAX_STR_LENGTH}`;
   } else if (value < MIN_STR_LENGTH) {
+    // console.log(`${value} меньше ${MIN_STR_LENGTH}`);
     strLengthEmpty.style.display = 'none';
     strLengthError.style.display = 'inline';
     strLength.classList.add('invalid');
     strLengthError.textContent = `Минимальное значение: ${MIN_STR_LENGTH}`;
   } else if (value > MAX_STR_LENGTH) {
+    // console.log(`${value} больше ${MAX_STR_LENGTH}`);
     strLengthEmpty.style.display = 'none';
     strLengthError.style.display = 'inline';
     strLength.classList.add('invalid');
     strLengthError.textContent = `Максимальное значение: ${MAX_STR_LENGTH}`;
   } else {
+    // console.log(`${value} валиден`);
     isValid = true;
     strLengthEmpty.style.display = 'none';
     strLengthError.style.display = 'none';
     strLength.classList.remove('invalid');
   }
 
-  userData.hasValidLength = isValid;
-  userData.strLength = value;
+  if (isValid) {
+    userData.hasValidLength = isValid;
+
+    if (Number.isInteger(value)) {
+      // console.log(`${value} целое число`);
+      userData.strLength = value;
+    } else {
+      // console.log(`${value} НЕ целое число`);
+      userData.strLength = Math.round(value);
+      event.target.value = userData.strLength;
+    }
+  }
 });
 
 
@@ -62,6 +77,16 @@ form.addEventListener('input', () => {
   generateBtn.disabled = true;
 
   userData.hasChar = cyrillic.checked || latin.checked || numbers.checked || special.checked;
+
+  // console.log(`
+  //   Данные пользователя: 
+  //   Выбрать все: ${selectAll.checked}
+  //   Кириллица: ${cyrillic.checked}
+  //   Латиница: ${latin.checked}
+  //   Цифры: ${numbers.checked}
+  //   Спецсимволы: ${special.checked}
+  //   Длина строки: ${userData.strLength}
+  // `);
 
   if (userData.hasChar && userData.hasValidLength) {
     generateBtn.disabled = false;
@@ -72,30 +97,29 @@ form.addEventListener('input', () => {
 // Слушатель кнопки "Сгенерировать"
 generateBtn.addEventListener('click', (event) => {
   event.preventDefault();
+  
+  copyIcon.style.display = 'inline';
+  checkIcon.style.display = 'none';
+  copyBtn.disabled = false;
 
-  if (!userData.hasChar || !userData.strLength) {
-    alert('Что-то пошло не так');
-    return;
-  }
-
-  let chars = '';
-
-  if (cyrillic.checked) chars += CHARS['cyrrilic'];
-  if (latin.checked) chars += CHARS['latin'];
-  if (special.checked) chars += CHARS['special'];
-  if (numbers.checked) chars += CHARS['numbers'];
-
-  // Генерация строки
   let resultStr = '';
 
-  for (let i = 0; i < userData.strLength; i++) {
-    const randomIndex = Math.floor(Math.random() * chars.length);
-    resultStr += chars[randomIndex];
-  }
+  if (!userData.hasChar || !userData.strLength) {
+    resultStr = 'Что-то пошло не так';
+  } else {
+    // Генерация строки
+    let chars = '';
 
-  checkIcon.style.display = 'none';
-  copyIcon.style.display = 'inline';
-  copyBtn.disabled = false;
+    if (cyrillic.checked) chars += CHARS['cyrrilic'];
+    if (latin.checked) chars += CHARS['latin'];
+    if (special.checked) chars += CHARS['special'];
+    if (numbers.checked) chars += CHARS['numbers'];
+    
+    for (let i = 0; i < userData.strLength; i++) {
+      const randomIndex = Math.floor(Math.random() * chars.length);
+      resultStr += chars[randomIndex];
+    }
+  }
 
   return result.textContent = resultStr;
 });
